a44b589612ba922668d9adf87d99b1666cc88dac
Alex Mikhailik <alex-under-2010@mail.ru>
2016-07-18 01:15:24 +0300

    improvements added

--- src/Main.java
+++ src/Main.java
@@ -31,22 +31,21 @@ public class Main {
         System.out.print("\nMon Tue Wed Thu Fri "); // default color
         System.out.println((char) 27 + "[31mSat Sun" + (char) 27 + "[0m"); // red color
 
-        int i, j, f = 1, r = 7, c = 7; // f = first day, r = rows, c = columns
+        int i, j, f = 1, r = 6, c = 7; // f = first day, r = rows, c = columns
         int[][] monthCalendar = new int[r][c]; // new array
         for (i = 0; i < r && f <= (curr.lengthOfMonth()); ++i)
             for (j = 0; j < c && f <= (curr.lengthOfMonth()); ++j)
-                if ((i == 0 && j > curr.get(ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR)) || i != 0) //loop
+                if ((i == 0 && j >= curr.get(ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR)) || i != 0) //loop
                     monthCalendar[i][j] = f++;
 
         for (i = 0; i < r; ++i, System.out.println())
             for (j = 0; j < c; ++j)
                 if (monthCalendar[i][j] != 0)
-                    if ( localDate.getMonth() == curr.getMonth() && monthCalendar[i][j] == curr.getDayOfMonth() ) { //check if the month equals current month
+                    if (localDate.getMonth() == curr.getMonth() && monthCalendar[i][j] == curr.get(ChronoField.DAY_OF_MONTH)) { //check if the selected month equals current month
                         System.out.printf((char) 27 + "[1;34m");
                         System.out.printf("%3d ",monthCalendar[i][j]);
                         System.out.printf((char) 27 + "[0m");
-                    }
-                    else if (j==5 || j==6) {
+                    } else if (j == 5 || j == 6) { // Sat and Sun colors
                         System.out.printf((char) 27 + "[31m");
                         System.out.printf("%3d ",monthCalendar[i][j]);
                         System.out.printf((char) 27 + "[0m");
@@ -55,6 +54,7 @@ public class Main {
                         System.out.printf("%3d ", monthCalendar[i][j]);
                 else
                     System.out.print("    "); // empty "dates" of foreign months
+        // information block stuff
         System.out.print("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n");
         System.out.print("Color meanings are shown below\n");
         System.out.print((char) 27 + "[1;34m"+"Current day " +(char) 27 + "[0m"+ "|");
